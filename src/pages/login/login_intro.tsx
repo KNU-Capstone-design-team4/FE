f310d157377b0323b45c859c7be5032834ea4358
whyyhyh <judynam03@gmail.com>
2025-12-02 16:36:40 +0900

    회원가입문구추가

--- src/pages/login/login_intro.tsx
+++ src/pages/login/login_intro.tsx
@@ -9,8 +9,7 @@ const LoginIntroPage: React.FC = () => {
         <img src="/lawbot_logo.svg" alt="LawBot Logo" className="auth-logo-img" style={{ textAlign: 'left', marginBottom: '60px', marginLeft: 0 }} />
         <h2>법률 문서를 쉽게 작성하세요!</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua.
+          LawBot과 함께라면 간단한 채팅을 통해 복잡한 법률 문서를 쉽게 작성하고 다운로드 할 수 있습니다. 지금 바로 시작해보세요!
         </p>
         <Link to="/register">
           <button className="auth-button">시작하기</button>
